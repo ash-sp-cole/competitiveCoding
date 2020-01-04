3c83af5afcba821688f5aaed1871712fcc00e82d
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-04 14:19:06 +0100

    : 8.3 MB, less than 100.00%

--- defangingIPAddressLeetCode/main.cpp
+++ defangingIPAddressLeetCode/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
-
+#include <string>
+#include <vector>
 
 using namespace std; 
 
@@ -8,10 +8,38 @@ using namespace std;
 int main () {
 
 
+string address {"1.1.1.1.2.4"};
+
+size_t n = address.size();
+char k{};
+string ip {};
+cout << " \n your address before is : " << address << " length is  " << n;
+
+
+// defangle()
+
+for (int i = 0; i < n; i++) {
+if (address[i] == '.') {
+
+   
+    ip+="[.]";
+   
+}
+else {
+    ip += address[i];
+}
+
+
+}
 
 
 
-cout << " test comp code ";
+
+
+cout << " \n \n your address after is  : " << ip;
+
+
+
 
 
 return 0;
