2950bf2f84149b0d2640246c2f655e052f84357b
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-05 11:26:52 +0100

    complete

---
+++ jewelsAndStones/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main () {
+
+
+string j {"z"};
+string s {"ZZ"};
+int count{};
+
+for (int i = 0; i < j.length(); i++){
+
+    char l = j.at(i);
+
+                    for (int k =0; k < s.length(); k++) {
+                        char x = s.at(k);
+                        if ( l == x) count++;
+                    }
+}
+
+cout << count ;
+
+
+    return 0;
+}
