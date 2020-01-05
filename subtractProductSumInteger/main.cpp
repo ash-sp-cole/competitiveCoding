7a99bf142399b9734ee7924b418ae2a4bd57c347
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-05 10:38:12 +0100

    complete 100 % pass

--- subtractProductSumInteger/main.cpp
+++ subtractProductSumInteger/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 
 
 using namespace std;
@@ -7,9 +7,40 @@ using namespace std;
 
 int main () {
 
-cout << " test comp";
+
+int num {4421};
+
+int sum {};
+
+int product {1};
+
+int digit{};
 
 
 
+cout << "  \n \n sum is " << endl;
+
+while (num != 0) {
+
+digit = num % 10;
+
+product = product *digit;
+num = num /10;
+
+sum +=digit;
+
+cout << " digit is : " <<  digit << " number with out digit now is :  " << num << endl; 
+
+
+
+}
+   
+cout << " \n \n sum of all digits is : " << sum << endl;
+
+cout << " \n \n \n product of all digits is  : " << product << endl;
+
+int answer = product - sum;
+
+cout  << " answer is : " << answer << endl;
     return 0;
 }
