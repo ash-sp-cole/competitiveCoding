e77dd6973c248b7d9f2a2526660c85c5237a67e5
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-08 19:45:30 +0100

    test

--- replaceElements/main.cpp
+++ replaceElements/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
+int main()
+{
 
-int main () {
+    int arr[]{17, 18, 5, 4, 6, 1};
 
+    size_t n = sizeof(arr) / sizeof(arr[0]);
 
+    int high = (n - 1);
+    int temp{};
+    for (int i = 0; i < n;)
+    {
 
-
-
-
-std::cout <<" test replace";
-
-
-
-
+        std::cout << " " << arr[i];
+        i++;
+    }
 
     return 0;
 }
