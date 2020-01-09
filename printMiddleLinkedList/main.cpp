587db5af87a803616e2fb16efb6f94d485615201
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-09 18:48:08 +0100

    linked list in class not struct

--- printMiddleLinkedList/main.cpp
+++ printMiddleLinkedList/main.cpp
@@ -1,28 +1,50 @@
 #include < iostream>
 
+class Node
+{
+
+public:
+    int data{};
+    Node *next;
 
 
+};
+
+int main()
+{
 
 
+Node *head = NULL;
+Node *second = NULL;
+Node *third = NULL;
+Node *fourth = NULL;
 
 
+head = new Node();
+second = new Node();
+third = new Node();
+fourth = new Node();
 
 
+head->data = 1;
+head-> next = third;
+
+second ->data = 2;
+second -> next = fourth;
+
+third -> data = 3;
+third -> next = second;
+
+fourth ->data = 4;
+fourth->next = NULL;
 
 
-int main () {
+while (head != NULL) {
+
+    std::cout << head->data << " ";
+    head = head->next;
 
 
-
-
-std::cout << " test print middle ";
-
-
-
-
-
-
-
-
+};
     return 0;
 }
