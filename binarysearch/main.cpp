b3265e94f25be33b378dde22fffe67b5390a6ec6
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-15 13:52:46 +0100

    add sort

--- binarysearch/main.cpp
+++ binarysearch/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-
+bool binarySearch (int arr[], int n, int low, int high);
 
 
 int main () {
@@ -11,23 +11,9 @@ size_t n = sizeof(arr) / sizeof(arr[0]);
 
 int low {0};
 int high = (n -1);
-int target {3};
+int target {6};
 
 
-
-while (low <= high) {
-
-int mid = (low + high) / 2;
-
-    if (arr[mid] == target) {
-std::cout << " your search target is found at index " << mid << "  value is : " << arr[mid] << std::endl;
-
-    }
-
-
-
-
-}
 
 
 
@@ -37,5 +23,46 @@ std::cout << " your search target is found at index " << mid << "  value is : " 
 
 
     return 0;
+
+}
+
+
+bool binarySearch (int arr[], int n, int low, int high) {
+int target{};
+
+std:: cout << " please enter your target value " << std::endl;
+
+std::cin >> target;
+
+std::cout << "\n \n \n" << std::endl;
+
+
+while (low <= high) {
+
+int mid = (low + high) / 2;
+
+    if (arr[mid] == target) {
+std::cout << " your search target is found at index " << mid << "  value is : " << arr[mid] << std::endl;
+    break;
+    return true;
+    }
+
+    else if (arr[mid] < target) {
+
+
+        low = (mid + 1);
+
+    }
+
+    else if (arr[mid] > target) {
+        high = (mid -1);
+    }
+
+
+}
+
+return false;
+
+
 
 }
