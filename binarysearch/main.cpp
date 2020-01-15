4601967dd81111f852ce9b41a6b22c79c154fd68
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-15 13:45:22 +0100

    update

--- binarysearch/main.cpp
+++ binarysearch/main.cpp
@@ -5,9 +5,29 @@
 
 int main () {
 
+int arr[] { 1,2,3,4,5,6,7,8,9,10};
+
+size_t n = sizeof(arr) / sizeof(arr[0]);
+
+int low {0};
+int high = (n -1);
+int target {3};
 
 
 
+while (low <= high) {
+
+int mid = (low + high) / 2;
+
+    if (arr[mid] == target) {
+std::cout << " your search target is found at index " << mid << "  value is : " << arr[mid] << std::endl;
+
+    }
+
+
+
+
+}
 
 
 
@@ -17,5 +37,5 @@ int main () {
 
 
     return 0;
-    
+
 }
